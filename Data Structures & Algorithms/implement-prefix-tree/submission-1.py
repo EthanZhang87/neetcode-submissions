b8bbb1ebe66ca3408c60af4c3b4e3909705ea5e2
Ethan Zhang <71947274+EthanZhang87@users.noreply.github.com>
2026-09-16 17:49:20 -0400

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.py
@@ -0,0 +1,53 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+class PrefixTree:
+
+    def __init__(self):
+        self.start = TrieNode()
+        
+
+        
+
+    def insert(self, word: str) -> None:
+        curr = self.start
+
+        for c in word:
+            if c in curr.children:
+                curr = curr.children[c]
+            else:
+                curr.children[c] = TrieNode()
+                curr = curr.children[c]
+
+        curr.endOfWord = True
+
+
+
+    def search(self, word: str) -> bool:
+        curr = self.start
+
+        for c in word:
+            if c not in curr.children:
+                return False
+
+            curr = curr.children[c]
+
+        return curr.endOfWord
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.start
+
+        for c in prefix:
+            if c not in curr.children:
+                return False
+
+            curr = curr.children[c]
+
+        return True
+
+
+        
+        
